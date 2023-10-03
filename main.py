05b297615ea16215a7afe00520e232e88ac25482
zasyya <76533662+zasyya@users.noreply.github.com>
2023-10-03 22:18:36 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print (meme_dict.keys)
+else:
+    print("KELİME BULUNAMADI")
+    input("kelimenizi lütfen büyük harflerle tırnak içinde yazdıktan sonra iki nokta koyup anlamını tekrar tırnak içine alarak yazınız(lütfen tırnak işaretini unutmayınız):")
+    a = input
+    
+    meme_dict == a + meme_dict
+    
