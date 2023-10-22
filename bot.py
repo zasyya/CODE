2bd3525aff3a8cad192c98b459871dac2ece9e83
zasyya <76533662+zasyya@users.noreply.github.com>
2023-10-22 19:30:30 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,65 @@
+import discord
+import random
+from sifreolusturucu import gen_pass
+
+# ayricaliklar (intents) değişkeni botun ayrıcalıklarını depolayacak
+intents = discord.Intents.default()
+# Mesajları okuma ayrıcalığını etkinleştirelim
+intents.message_content = True
+# client (istemci) değişkeniyle bir bot oluşturalım ve ayrıcalıkları ona aktaralım
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send('Selam! Ben bir botum!')
+    elif message.content.startswith('$smile'):
+        await message.channel.send(emoji_olusturucu())
+    elif message.content.startswith('$coin'):
+        await message.channel.send(yazi_tura())
+    elif message.content.startswith('$pass'):
+        await message.channel.send(sifre_olusturucu(10))
+    else:
+        await message.channel.send("Bu komutu anlayamadım :(")
+
+client.run(settings["TOKEN"])
+def emoji_olusturucu():
+    emoji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    return random.choice(emoji)
+
+
+def yazi_tura():
+    para = random.randint(0, 2)
+    if para == 0:
+        return "YAZI"
+    else:
+        return "TURA"
+
+def sifre_olusturucu(sifre_uzunlugu):
+    ogeler = "+-/*!&$#?=@<>"
+    sifre = ""
+
+def double_letter(str):
+    # Sonuç
+    result = ''
+    for letter in str:
+        result += letter * 2
+    return result
+
+def secret_function(a, b):
+    # Kodunuzu buraya yazın!
+    return " "
+
+print()
+print(secret_function(1, 2))
+print(secret_function("Merhaba, ", "Dünya!"))
+
+
+client.run("MTE2MzkwODU1MDAyMDU4MzUzNA.GaGjOd.JzpGZeHEQpB3h68dJtdQ4T5d3q2ESF32j1MVBk")
